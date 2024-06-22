021051531c274a7f1f3e269ffc6acda18cfaa982
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-06-22 20:09:16 +0530

    feat: send message api route

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,53 @@
+import dbConnect from "@/config/db-connect";
+import UserModel from "@/model/user";
+import { Message } from "@/model/user";
+
+export async function POST(request:Request){
+    await dbConnect();
+
+    const {username, content}=await request.json();
+
+    try {
+       const user = await UserModel.findOne({username});
+       if(!user){
+           return Response.json({
+            sucess:false,
+            message:"User not found"
+          },{
+            status:404
+          });
+       }
+
+       if(!user.isAcceptingMessage){
+        return Response.json({
+            sucess:false,
+            message:"User not accepting messages"
+          },{
+            status:403
+          })
+       }
+
+       const newMessage = {content,createdAt: new Date()};
+
+       user.messages.push(newMessage as Message);
+       await user.save();
+
+       return Response.json({
+        sucess:true,
+        message:"Message sent successfully"
+      },{
+        status:200
+      })
+
+    } catch (error) {
+        console.log("Error in sending messages");
+        return Response.json({
+            sucess:false,
+            message:"Internal server error"
+          },{
+            status:500
+          })
+        
+    }
+
+}
