498c46a44ac756219f95060da0a858b79b064f24
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-06-25 18:17:33 +0530

    style: footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="w-full max-w-7xl mx-auto py-10">
       <div className="flex flex-col items-center gap-4 rounded-lg bg-[#002D62] p-6 shadow-lg sm:flex-row sm:justify-between">
         <strong className="text-xl text-white sm:text-xl">
-          Make Your Next Move !
+          Make Your Next Move With the <Button> NEXT.js</Button>
         </strong>
 
         <a href="https://www.linkedin.com/in/dalpatrathore">
